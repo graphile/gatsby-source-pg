ae57a574ab77ae4420a21fbf28249fe3ba234b4d
Benjie Gillam <benjie@jemjie.com>
2021-11-24 15:41:03 +0000

    Gatsby v4 support

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,28 +4,33 @@ const { Pool } = require("pg");
 const createSchema = require("./lib/createSchema");
 const PostGraphileLink = require("./lib/PostGraphileLink");
 
-exports.sourceNodes = async (
-  utils,
-  {
+exports.createSchemaCustomization = async (props, options) => {
+  const {
     typeName = "PostGraphile",
     fieldName = "postgres",
     refetchInterval,
-    ...options
-  },
-) => {
-  const { connectionString, schema: postgresSchema, ...rest } = options;
+    connectionString,
+    schema: postgresSchema,
+    ...rest
+  } = options;
 
   const pool = new Pool({
     connectionTimeoutMillis: 30 * 1000,
     connectionString,
   });
+
   const graphqlSchema = await createSchema(pool, postgresSchema, rest);
 
-  await gatsbySourceGraphQLNode.sourceNodes(utils, {
+  return gatsbySourceGraphQLNode.createSchemaCustomization(props, {
+    ...rest,
     typeName,
     fieldName,
     refetchInterval,
     createLink: () => new PostGraphileLink({ pool, schema: graphqlSchema }),
     createSchema: () => graphqlSchema,
   });
+};
+
+exports.sourceNodes = async (utils, options) => {
+  await gatsbySourceGraphQLNode.sourceNodes(utils, options);
 };
